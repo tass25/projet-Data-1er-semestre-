42babbf1cdd851697679611e77b0cfed755cb7a5
Tesnime_Ellabou <101891297+tass25@users.noreply.github.com>
2024-10-06 09:24:16 +0000

    ETA the assistant

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,22 @@ import streamlit as st
 with st.container(): 
     st.subheader('Hello!')
     st.write("---")
+
+with st.container():
+    col1, col2 = st.columns(2)
+    with col1:
+             st.subheader("Meet ETA,")
+             st.title('Your Assistant')
+             st.write("##")
+             st.write("______________________________________________")
+             st.subheader(
+                 """
+                    ETA predicts heart attack risk based on reported symptoms.
+
+
+                 """)
+
+    with col2:
+             st.markdown("![Alt Text](https://bit.ly/anna-find)")
+             
+           
